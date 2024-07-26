06dc90f36f8719aa6b5372c2bb4060fa024f5120
deniPamungkas <pamungkasdeni18@gmail.com>
2024-07-26 14:21:50 +0700

    sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -18,107 +18,222 @@ const SideBar = () => {
     setValue(e.target.parentElement.id);
   };
   return (
-    <div
-      className={`w-[200px] md:w-[250px] h-screen bg-white p-3 lg:p-5 absolute left-0 ${
-        position ? " translate-x-[0px]" : "-translate-x-[200px]"
-      } md:block md:relative md:left-0 z-50 ease-in-out transition`}
-    >
-      <div className="flex justify-between items-center">
-        <span>
-          <h3 className="text-sm">{todayDate()}</h3>
-          <h1 className="text-2xl font-bold">Hi, Joglo</h1>
-        </span>
-        <button
-          type="button"
-          className="md:hidden"
-          onClick={() => {
-            setPosition(false);
-          }}
-        >
-          <ArrowBackIcon />
-        </button>
+    <>
+      <div
+        className={`w-[200px] md:w-[250px] md:hidden h-screen bg-white p-3 lg:p-5 absolute ${
+          position ? "translate-x-[0px]" : "-translate-x-[200px]"
+        } md:block md:relative z-50 ease-in-out transition`}
+      >
+        <div className="flex justify-between items-center">
+          <span>
+            <h3 className="text-sm">{todayDate()}</h3>
+            <h1 className="text-2xl font-bold">Hi, Joglo</h1>
+          </span>
+          <button
+            type="button"
+            className="md:hidden"
+            onClick={() => {
+              setPosition(false);
+            }}
+          >
+            <ArrowBackIcon />
+          </button>
+        </div>
+        <ul className="flex flex-col gap-y-5 py-10">
+          <Link
+            to={"/admin"}
+            onClick={handleClick}
+            id="Dashboard"
+            className={`${
+              value == "Dashboard" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <DashboardIcon
+              style={
+                value == "Dashboard" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Dashboard</span>
+          </Link>
+          <Link
+            to={"products"}
+            onClick={handleClick}
+            id="Products"
+            className={`${
+              value == "Products" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <ShoppingCartIcon
+              style={
+                value == "Products" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Products</span>
+          </Link>
+          <Link
+            to={"sales"}
+            onClick={handleClick}
+            id="Sales"
+            className={`${
+              value == "Sales" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <SsidChartIcon
+              style={value == "Sales" ? { color: "white" } : { color: "gray" }}
+            />
+            <span>Sales</span>
+          </Link>
+          <Link
+            to={"playstation"}
+            onClick={handleClick}
+            id="Playstation"
+            className={`${
+              value == "Playstation"
+                ? "bg-blue-500 text-white"
+                : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <SportsEsportsIcon
+              style={
+                value == "Playstation" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Playstation</span>
+          </Link>
+          <Link
+            to={"/"}
+            onClick={() => {
+              setPosition(false);
+            }}
+            id="Home"
+            className={`bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <HomeIcon style={{ color: "white" }} />
+            <span>Home</span>
+          </Link>
+          <Link
+            to={"/logout"}
+            id="Logout"
+            className={` bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <LogoutIcon style={{ color: "white" }} />
+            <span>Log Out</span>
+          </Link>
+        </ul>
       </div>
-      <ul className="flex flex-col gap-y-5 py-10">
-        <Link
-          to={"/admin"}
-          onClick={handleClick}
-          id="Dashboard"
-          className={`${
-            value == "Dashboard" ? "bg-blue-500 text-white" : "text-gray-500"
-          } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <DashboardIcon
-            style={
-              value == "Dashboard" ? { color: "white" } : { color: "gray" }
-            }
-          />
-          <span>Dashboard</span>
-        </Link>
-        <Link
-          to={"products"}
-          onClick={handleClick}
-          id="Products"
-          className={`${
-            value == "Products" ? "bg-blue-500 text-white" : "text-gray-500"
-          } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <ShoppingCartIcon
-            style={value == "Products" ? { color: "white" } : { color: "gray" }}
-          />
-          <span>Products</span>
-        </Link>
-        <Link
-          to={"sales"}
-          onClick={handleClick}
-          id="Sales"
-          className={`${
-            value == "Sales" ? "bg-blue-500 text-white" : "text-gray-500"
-          } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <SsidChartIcon
-            style={value == "Sales" ? { color: "white" } : { color: "gray" }}
-          />
-          <span>Sales</span>
-        </Link>
-        <Link
-          to={"playstation"}
-          onClick={handleClick}
-          id="Playstation"
-          className={`${
-            value == "Playstation" ? "bg-blue-500 text-white" : "text-gray-500"
-          } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <SportsEsportsIcon
-            style={
-              value == "Playstation" ? { color: "white" } : { color: "gray" }
-            }
-          />
-          <span>Playstation</span>
-        </Link>
-        <Link
-          to={"/"}
-          id="Home"
-          className={`bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <HomeIcon style={{ color: "white" }} />
-          <span>Home</span>
-        </Link>
-        <Link
-          to={"/logout"}
-          id="Logout"
-          className={` bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
-        >
-          <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
-          <LogoutIcon style={{ color: "white" }} />
-          <span>Log Out</span>
-        </Link>
-      </ul>
-    </div>
+      <div
+        className={`hidden md:block w-[250px] h-screen bg-white p-3 lg:p-5 z-50`}
+      >
+        <div className="flex justify-between items-center">
+          <span>
+            <h3 className="text-sm">{todayDate()}</h3>
+            <h1 className="text-2xl font-bold">Hi, Joglo</h1>
+          </span>
+          <button
+            type="button"
+            className="md:hidden"
+            onClick={() => {
+              setPosition(false);
+            }}
+          >
+            <ArrowBackIcon />
+          </button>
+        </div>
+        <ul className="flex flex-col gap-y-5 py-10">
+          <Link
+            to={"/admin"}
+            onClick={handleClick}
+            id="Dashboard"
+            className={`${
+              value == "Dashboard" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <DashboardIcon
+              style={
+                value == "Dashboard" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Dashboard</span>
+          </Link>
+          <Link
+            to={"products"}
+            onClick={handleClick}
+            id="Products"
+            className={`${
+              value == "Products" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <ShoppingCartIcon
+              style={
+                value == "Products" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Products</span>
+          </Link>
+          <Link
+            to={"sales"}
+            onClick={handleClick}
+            id="Sales"
+            className={`${
+              value == "Sales" ? "bg-blue-500 text-white" : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <SsidChartIcon
+              style={value == "Sales" ? { color: "white" } : { color: "gray" }}
+            />
+            <span>Sales</span>
+          </Link>
+          <Link
+            to={"playstation"}
+            onClick={handleClick}
+            id="Playstation"
+            className={`${
+              value == "Playstation"
+                ? "bg-blue-500 text-white"
+                : "text-gray-500"
+            } rounded-lg h-[50px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <SportsEsportsIcon
+              style={
+                value == "Playstation" ? { color: "white" } : { color: "gray" }
+              }
+            />
+            <span>Playstation</span>
+          </Link>
+          <Link
+            to={"/"}
+            onClick={() => {
+              setPosition(false);
+            }}
+            id="Home"
+            className={`bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <HomeIcon style={{ color: "white" }} />
+            <span>Home</span>
+          </Link>
+          <Link
+            to={"/logout"}
+            id="Logout"
+            className={` bg-gray-500 text-white rounded-lg h-[40px] w-full flex justify-start px-3 font-semibold items-center gap-x-3 relative cursor-pointer`}
+          >
+            <div className="absolute top-0 right-0 left-0 bottom-0 z-10"></div>
+            <LogoutIcon style={{ color: "white" }} />
+            <span>Log Out</span>
+          </Link>
+        </ul>
+      </div>
+    </>
   );
 };
 
