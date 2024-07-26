b6d9a573efc6eb9ecc9d50c66e1cc56625191a66
deniPamungkas <pamungkasdeni18@gmail.com>
2024-07-26 14:37:49 +0700

    home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,32 +8,34 @@ import LoginIcon from "@mui/icons-material/Login";
 const Home = () => {
   const { value } = useContext(Side);
   return (
-    <div className="w-full h-screen flex-col md:flex-row flex justify-center items-center gap-3">
-      <Link to={"/pesanan"}>
-        <div className="w-[170px] h-[170px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#eb12e3] to-[#e86f66] font-semibold text-2xl text-white">
-          <MenuBookIcon fontSize="large" />
-          <span>Pesanan</span>
-        </div>
-      </Link>
-      <Link
-        to={
-          (value == "Dashboard" && "/admin") ||
-          (value == "Products" && "/admin/products") ||
-          (value == "Sales" && "/admin/sales") ||
-          (value == "Playstation" && "/admin/playstation")
-        }
-      >
-        <div className="w-[170px] h-[170px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#eaff81] to-[#e8dd66] font-semibold text-2xl text-white">
-          <DashboardIcon fontSize="large" />
-          <span>Dashboard</span>
-        </div>
-      </Link>
-      <Link to={"/login"}>
-        <div className="w-[170px] h-[170px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#23c4ea] to-[#667ee8] font-semibold text-2xl text-white">
-          <LoginIcon fontSize="large" />
-          <span>Login</span>
-        </div>
-      </Link>
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="m-auto w-fit grid grid-cols-2 gap-2 md:gap-3">
+        <Link to={"/pesanan"}>
+          <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#eb12e3] to-[#e86f66] font-semibold text-xl text-white">
+            <MenuBookIcon fontSize="large" />
+            <span>Pesanan</span>
+          </div>
+        </Link>
+        <Link
+          to={
+            (value == "Dashboard" && "/admin") ||
+            (value == "Products" && "/admin/products") ||
+            (value == "Sales" && "/admin/sales") ||
+            (value == "Playstation" && "/admin/playstation")
+          }
+        >
+          <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#eaff81] to-[#e8dd66] font-semibold text-xl text-white">
+            <DashboardIcon fontSize="large" />
+            <span>Dashboard</span>
+          </div>
+        </Link>
+        <Link to={"/login"}>
+          <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#23c4ea] to-[#667ee8] font-semibold text-xl text-white">
+            <LoginIcon fontSize="large" />
+            <span>Login</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
