518c10a960b96a0fcc5c37c9c2fd1f92cc0016e2
deniPamungkas <pamungkasdeni18@gmail.com>
2024-07-29 21:33:37 +0700

    login confirm

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,47 @@
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { Side } from "../context/sideContext";
 import LoginIcon from "@mui/icons-material/Login";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
   const { value } = useContext(Side);
+  const navigate = useNavigate();
+
+  const confilrmLogin = useQuery({
+    queryFn: async () => {
+      try {
+        const isLoggedIn = axios.get(
+          "https://joglo-ndeso-kasir-api-dev.vercel.app/auth/is-logged-in",
+          { withCredentials: true }
+        );
+        return isLoggedIn;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    },
+    queryKey: ["confirmLogin"],
+  });
+
+  const handleLogout = async () => {
+    try {
+      const logout = axios.post(
+        "https://joglo-ndeso-kasir-api-dev.vercel.app/auth/logout",
+        {},
+        { withCredentials: true }
+      );
+      navigate("/login");
+      return logout;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  };
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="m-auto w-fit grid grid-cols-2 gap-2 md:gap-3">
@@ -29,12 +64,22 @@ const Home = () => {
             <span>Dashboard</span>
           </div>
         </Link>
-        <Link to={"/login"}>
-          <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#23c4ea] to-[#667ee8] font-semibold text-xl text-white">
+        {confilrmLogin?.data == undefined ? (
+          <Link to={"/login"}>
+            <div className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#23c4ea] to-[#667ee8] font-semibold text-xl text-white">
+              <LoginIcon fontSize="large" />
+              <span>Login</span>
+            </div>
+          </Link>
+        ) : (
+          <div
+            onClick={handleLogout}
+            className="w-[150px] h-[150px] md:w-[200px] md:h-[200px] shadow hover:shadow-2xl rounded-lg flex flex-col justify-center items-center bg-gradient-to-t from-[#23c4ea] to-[#667ee8] font-semibold text-xl text-white"
+          >
             <LoginIcon fontSize="large" />
-            <span>Login</span>
+            <span>Logout</span>
           </div>
-        </Link>
+        )}
       </div>
     </div>
   );
